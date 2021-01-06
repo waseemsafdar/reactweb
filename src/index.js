3dc1044f3adcf176a37961baac07e5e9b901a853
HP <waseemsafdar98@gmail.com>
2021-01-06 18:50:49 +0500

    after all new

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import * as serviceWorker from './serviceWorker';
 import {createStore,applyMiddleware} from 'redux';
 import rootReducers from './store/reducers/rootReducer'
 import {Provider} from 'react-redux';
-import thunk from 'redux-thunk '
+import thunk from 'redux-thunk';
 
-const store= createStore(rootReducers. applyMiddleware(thunk));
+
+const store= createStore(rootReducers ,applyMiddleware(thunk));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
